e971202e570b87ef1b129709658ce04c8a85ead8
Rossella Sonzogni <ross.sonzogni@gmail.com>
2024-04-26 10:39:22 +0200

    modified Counter component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
 function Counter({initialValue = 0}) {
   const [counter, setCounter] = useState(initialValue);
+
+  const directionRef = useRef(null);
+  const prevCounterRef = useRef(null);
   
   useEffect(() => {
     console.log(`The value of the counter is ${counter}`)
+    if (prevCounterRef.current !== null) {
+      if (counter > prevCounterRef.current) {
+        directionRef.current = 'up';
+      } else if (counter < prevCounterRef.current) {
+        directionRef.current = 'down';
+      }
+    }
+    prevCounterRef.current = counter;
   }, [counter])
+
+  useEffect(() => {
+    if (directionRef.current !== null) {
+      console.log(`Direction of change: ${directionRef.current}`);
+    }
+  }, [directionRef.current]);
 
   function handleCounterIncrement() {
     setCounter((c) => c + 1)
