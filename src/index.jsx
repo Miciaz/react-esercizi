bbabbdac9f51d01ff13374c1df22d66a31cbb149
Rossella Sonzogni <ross.sonzogni@gmail.com>
2024-05-14 10:25:46 +0200

    iniziato esercizio router

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
 import { StrictMode } from "react";
 import './index.css'
+import { Root } from "./Root";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>
 );
