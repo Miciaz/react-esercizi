bd979701a893ff654926afd4814068edac324974
Rossella Sonzogni <ross.sonzogni@gmail.com>
2024-06-27 16:47:20 +0200

    fix: fixed gitUser

--- src/Data-fetching/GithubUser.jsx
+++ src/Data-fetching/GithubUser.jsx
@@ -1,27 +1,41 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
-export  function GithubUser({username}) {
-    const [data, setData] = useState(null)
+function GitHubUser({ username }) {
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${username}`)
-        .then(response => {
-            return response.json()
-        })
-        .then(json => {
-            console.log(json)
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${username}`
+        );
+        const data = await response.json();
+        setUser(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
 
-            setData(json)
-        })
-    }, [username])
+    fetchUserData();
+  }, [username]);
 
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div>
-            {data && <h1>{data.name}</h1>}
-            {data && <p>{data.login}</p>}
-            {data && <img src={data.avatar_url} alt="immagine avatar" />}
-        </div>
-    )
-
+  return (
+    <div>
+      <h2>Name: {user.name}</h2>
+      <p>Login: {user.login}</p>
+      <img src={user.avatar_url} alt="User Avatar" />
+    </div>
+  );
 }
+
+GitHubUser.propTypes = {
+  username: PropTypes.string.isRequired,
+};
+
+export default GitHubUser;
